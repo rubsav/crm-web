de6a18f8f1ae6f8726f7d65fbb5f69d54f142458
Ruben Avila <Ruavilaa@hotmail.com>
2014-10-29 14:48:38 -0400

    created a layout that yields to the views

--- crm.rb
+++ crm.rb
@@ -21,13 +21,8 @@ get '/contacts/new' do
 end
 
 
-
-
-
-
-# post '/contacts' do
-# 	puts PARAMS: #{params}
-# 	contact = Conact.new (params [:first_name], params[:last_name])
-# 	$rolodex.add_contact(contact)
-# 	redirect to('/contacts')
-#end
+post '/contacts' do
+  new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
+	$rolodex.add_contact(new_contact)
+	redirect to('/contacts')
+end
