889b0247d1361bdb45e82fd778615bb18fd4722e
Ruben Avila <Ruavilaa@hotmail.com>
2014-10-30 14:34:20 -0400

    Created a new route that handles a remove request inside the show_contact form

--- crm.rb
+++ crm.rb
@@ -39,6 +39,32 @@ get "/contacts/:id/edit" do
 	end
 end
 
+put "/contacts/:id" do
+	@contact = @@rolodex.find(params[:id].to_i)
+	if @contact
+		@contact.first_name = params[:first_name]
+		@contact.last_name = params[:last_name]
+		@contact.email = params[:email]
+		@contact.note = params[:note]
+
+		redirect to("/contacts")
+	else
+		raise Sinatra::NotFound
+	end
+end
+
+delete "/contacts/:id" do
+	@contact = $rolodex.find(params[:id].to_i)
+	if @contact
+		$rolodex.delete_contact(@contact)
+		redirect to("/contacts")
+	else
+		raise Sinatra::NotFound
+	end
+end
+
+
+
 post '/contacts' do
 	new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
 	$rolodex.add_contact(new_contact)
